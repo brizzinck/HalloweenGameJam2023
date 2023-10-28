89a89a58750b3cb2745b10a3c563ef814e8c7b2f
Vlad Lavrishko <lavrishkovlad@gmail.com>
2023-10-28 17:40:47 +0300

    Add kill police

---
+++ Assets/CodeBase/NPC/NPCPoliceKill.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace CodeBase.NPC
+{
+  public class NPCPoliceKill : MonoBehaviour
+  {
+    [SerializeField] private int _touch = 3;
+    private void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D point = Physics2D.OverlapPoint(clickPosition);
+        if (point.TryGetComponent(out NPCPoliceKill npcPoliceKill))
+        {
+          npcPoliceKill._touch--;
+          if (_touch <= 0)
+            Destroy(npcPoliceKill.gameObject);
+        }
+      }
+    }
+  }
+}
