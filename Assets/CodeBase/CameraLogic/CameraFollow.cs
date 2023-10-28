8c3b267953418235697febfabd01cc4e086cf1df
sema2234 <91981026+sema2234@users.noreply.github.com>
2023-10-28 21:22:21 +0300

    1.3 mapCommit
    added map

--- Assets/CodeBase/CameraLogic/CameraFollow.cs
+++ Assets/CodeBase/CameraLogic/CameraFollow.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
-
+using Cinemachine;
 namespace CodeBase.CameraLogic
 {
   public class CameraFollow : MonoBehaviour
   {
+        [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
     [SerializeField] private float _distance;
     [SerializeField] private float _offsetY;
-
+    
     private Transform _following;
+        /*private void LateUpdate()
+        {
+          if (_following == null)
+            return;
+          Vector3 position = new Vector3(0, 0, -_distance) + FollowingPointPosition();
+          transform.position = position;
+        }*/
 
-    private void LateUpdate()
+    public void Follow(GameObject following)
     {
-      if (_following == null)
-        return;
-      Vector3 position = new Vector3(0, 0, -_distance) + FollowingPointPosition();
-      transform.position = position;
+            cinemachineVirtualCamera.Follow = following.transform;
     }
-
-    public void Follow(GameObject following) => 
-      _following = following.transform;
     private Vector3 FollowingPointPosition()
     {
       Vector3 followingPosition = _following.position;
