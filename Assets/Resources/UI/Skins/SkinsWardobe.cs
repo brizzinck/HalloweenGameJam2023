b3393125b58f6b645a8adec6588c2c9238246419
sema2234 <91981026+sema2234@users.noreply.github.com>
2023-10-29 16:59:06 +0300

    test1

---
+++ Assets/Resources/UI/Skins/SkinsWardobe.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPCSkin", menuName = "SkinLook", order = 51)]
+public class SkinsWardobe : ScriptableObject
+{
+    public Sprite NPCarm;
+}
